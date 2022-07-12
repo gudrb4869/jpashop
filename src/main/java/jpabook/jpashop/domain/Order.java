6825b10c11b1e622d91bf708040db652f5689496
HyungKyuPark <gudrb4869@naver.com>
2022-07-12 20:37:34 +0900

    2022-07-12 도메인 분석 설계

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,59 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.FetchType.*;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    /**
+     * fetch 전략 EAGER로 되있는 경우
+     * JPQL로 select o from orders o -> SQL select * from orders; 전체 주문내역 가져오는 쿼리 하나 실행시 member에대한 쿼리 n개 추가 실행
+     * 되는 n + 1 문제 발생할수 있음!
+     */
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate; //주문시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus; //주문상태 (ORDER, CANCEL)
+
+    //==연관관계 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+}
