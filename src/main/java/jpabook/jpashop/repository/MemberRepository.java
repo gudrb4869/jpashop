6a07cdebbbbddf98b7a7bfd7390db502a5fb12fb
HyungKyuPark <gudrb4869@naver.com>
2022-07-19 17:47:11 +0900

    2022-07-19 스프링 데이터 JPA 소개

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,38 +1,12 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 
-@Repository
-@RequiredArgsConstructor
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    /**
-     *    스프링 데이터 JPA 덕분에 @PersistenceContext 대신 @Autowired 써도 지원이 됨.
-     *    따라서 final 필드로 해주고 롬복의 @RequiredArgsConstructor 사용한 의존관계 주입이 가능함!
-      */
-    private final EntityManager em;
-
-    public void save(Member member) {
-        em.persist(member);
-    }
-
-    public Member findOne(Long id) {
-        return em.find(Member.class, id);
-    }
-
-    public List<Member> findAll() {
-        return em.createQuery("select m from Member m", Member.class)
-                .getResultList();
-    }
-
-    public List<Member> findByName(String name) {
-        return em.createQuery("select m from Member m where m.name = :name", Member.class)
-                .setParameter("name", name)
-                .getResultList();
-    }
+    //select m from Member m where m.name = :name
+    List<Member> findByName(String name);
 }
